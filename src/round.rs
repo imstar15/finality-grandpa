28e8354402498f8442ae8764b5f040dff515c82f
imstar15 <imstar15@qq.com>
2021-09-07 19:02:02 +0800

    Add logs

--- src/round.rs
+++ src/round.rs
@@ -343,6 +343,9 @@ impl<Id, H, N, Signature> Round<Id, H, N, Signature> where
 		}
 
 		self.update();
+
+		println!("self.update");
+
 		import_result.equivocation = equivocation;
 		Ok(import_result)
 	}
@@ -598,9 +601,7 @@ impl<Id, H, N, Signature> Round<Id, H, N, Signature> where
 				self.graph.find_ghost(Some((b_hash, b_num)), possible_to_precommit)
 					.map_or(true, |x| x == (g_hash, g_num))
 			}
-		});
-
-		crate::std::println!("self.completable");
+		})
 	}
 
 	/// Fetch the "round-estimate": the best block which might have been finalized
